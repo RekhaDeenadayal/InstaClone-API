e134703e0df41bbf87d7b4e13b88fa1b237b64cc
RekhaDeenadayal <80333356+RekhaDeenadayal@users.noreply.github.com>
2023-03-26 10:58:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const atls_url = process.env.MONGODB_URI;
-
+console.log(atls_url);
 mongoose.connect(atls_url,{
     useNewUrlParser : true,
     useUnifiedTopology : true
