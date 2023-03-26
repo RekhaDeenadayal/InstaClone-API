ae19c0763b9c081a1cc7c5a29dcfda0bf12ade5b
RekhaDeenadayal <bhagyarekha72748@gmail.com>
2023-03-26 10:22:31 +0530

    Added InstaClone Posts API

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const upload = multer({ dest: 'uploads/' });
+
+var cors = require('cors');
+const postController = require("./controller.js/postController")
+const port = 8080;
+
+const app = express();
+app.use(cors());
+app.use("/uploads", express.static('uploads'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const atls_url = "mongodb+srv://bhagyarekha72748:Rekha@cluster1.04pulm6.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(atls_url,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+}).then(()=>{
+    console.log("Connected to Cloud MongoDB ATLAS");
+})
+
+
+app.post("/api/image", upload.single("postImage"), postController.addPost)
+
+
+app.get("/api/image", postController.getPosts);
+
+app.listen(port, () => {
+    console.log(`Listening at port ${port}`)
+})
