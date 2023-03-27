090b8793f7b34d34e2ae369225c63856aabddc0a
RekhaDeenadayal <80333356+RekhaDeenadayal@users.noreply.github.com>
2023-03-27 12:32:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const multer = require("multer");
-const upload = multer({ dest: 'uploads/' });
-
 var cors = require('cors');
+
 const postController = require("./controller.js/postController")
 const port = 8080;
 
 const app = express();
 app.use(cors());
-app.use("/uploads", express.static('uploads'))
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const atls_url = process.env.MONGODB_URI;
-console.log(atls_url);
+const atls_url = "mongodb+srv://bhagyarekha72748:Rekha@cluster1.04pulm6.mongodb.net/?retryWrites=true&w=majority";
+
 mongoose.connect(atls_url,{
     useNewUrlParser : true,
     useUnifiedTopology : true
@@ -24,7 +21,7 @@ mongoose.connect(atls_url,{
 })
 
 
-app.post("/api/image", upload.single("postImage"), postController.addPost)
+app.post("/api/image", postController.addPost)
 
 
 app.get("/api/image", postController.getPosts);
